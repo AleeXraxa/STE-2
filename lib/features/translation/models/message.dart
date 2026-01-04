5fa5560fbe02fbc3b4ca210b482d05a67c11e071
Ali Raza <abbasia444@yahoo.com>
2026-01-04 15:30:53 +0500

    chat working

--- lib/features/translation/models/message.dart
+++ lib/features/translation/models/message.dart
@@ -2,6 +2,11 @@ class Message {
   final String text;
   final String language;
   final bool isUser;
+  final bool isPartial;
 
-  Message({required this.text, required this.language, required this.isUser});
+  Message(
+      {required this.text,
+      required this.language,
+      required this.isUser,
+      this.isPartial = false});
 }
